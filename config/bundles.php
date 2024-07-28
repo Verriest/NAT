d44d5d4a5c5e562acc85d4e92b369e6f21f3d2e6
Maxime Verriest <maxime.verriest@quadra-informatique.fr>
2024-07-28 19:12:22 +0200

    init home page install npm webpack

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
